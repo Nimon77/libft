be6c906b1b71a4d192e8182d2a856b02092bbc1d
Nicolas SIMON <nsimon@student.42.fr>
2019-11-15 15:47:43 +0100

    opti putstr_fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -16,9 +16,10 @@ void	ft_putstr_fd(char *s, int fd)
 {
 	int		i;
 
-	if (s == NULL)
-		return ;
-	i = 0;
-	while (s[i] != '\0')
-		write(fd, &s[i++], 1);
+	if (s)
+	{
+		i = 0;
+		while (s[i] != '\0')
+			write(fd, &s[i++], 1);
+	}
 }
